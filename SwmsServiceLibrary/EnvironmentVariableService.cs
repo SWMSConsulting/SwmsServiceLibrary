138eda5d0e33643335203528b2255fe7f994e650
bussa92 <buss@swms.de>
2025-02-04 11:05:49 +0100

    Add project files.

---
+++ SwmsServiceLibrary/EnvironmentVariableService.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SwmsServiceLibrary
+{
+    public class EnvironmentVariableService
+    {
+        private static readonly string _configPath = "appsettings.json";
+
+        public EnvironmentVariableService()
+        {
+            EnsureConfigFileExists();
+        }
+
+        private void EnsureConfigFileExists()
+        {
+            if (!File.Exists(_configPath))
+            {
+                var defaultConfig = new JObject(new JObject());
+                File.WriteAllText(_configPath, defaultConfig.ToString());
+            }
+        }
+
+        public static string GetSavedEnvironmentVariable(string key)
+        {
+            if (File.Exists(_configPath))
+            {
+                var config = JObject.Parse(File.ReadAllText(_configPath));
+                return config["EnvironmentVariables"]?[key]?.ToString();
+            }
+            return null;
+        }
+
+        public static string GetEnvironmentVariable(string variableName)
+        {
+            var savedValue = GetSavedEnvironmentVariable(variableName);
+            return savedValue ?? GetRequiredStringFromENV(variableName);
+        }
+
+        public static void SaveEnvironmentVariable(string key, string value)
+        {
+            var config = JObject.Parse(File.ReadAllText(_configPath));
+
+            if (config["EnvironmentVariables"] == null)
+            {
+                config["EnvironmentVariables"] = new JObject();
+            }
+
+            config["EnvironmentVariables"][key] = value;
+            File.WriteAllText(_configPath, config.ToString());
+        }
+
+        public void LoadFromEnvironment()
+        {
+            var config = JObject.Parse(File.ReadAllText(_configPath));
+
+            if (config["EnvironmentVariables"] == null)
+            {
+                config["EnvironmentVariables"] = new JObject();
+            }
+
+            foreach (var envVar in Environment.GetEnvironmentVariables().Keys)
+            {
+                string key = envVar.ToString();
+                string value = Environment.GetEnvironmentVariable(key);
+                config["EnvironmentVariables"][key] = value;
+            }
+
+            File.WriteAllText(_configPath, config.ToString());
+        }
+        public static int GetRequiredIntFromENV(string variableName)
+        {
+            var intString = Environment.GetEnvironmentVariable(variableName);
+            return intString == null ? throw new ArgumentException($"ENV variable {variableName} is missing!") : int.Parse(intString);
+        }
+
+        public static bool GetRequiredBoolFromENV(string variableName)
+        {
+            var boolString = Environment.GetEnvironmentVariable(variableName);
+            return boolString == null ? throw new ArgumentException($"ENV variable {variableName} is missing!") : bool.Parse(boolString);
+        }
+
+        public static string GetRequiredStringFromENV(string variableName)
+        {
+            var envString = Environment.GetEnvironmentVariable(variableName);
+            return envString == null ? throw new ArgumentException($"ENV variable {variableName} is missing!") : envString;
+        }
+
+    }
+}
